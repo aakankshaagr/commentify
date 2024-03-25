375729925925abddaa9edc1a8dd980ed269bc96d
Aakanksha Agrawal <aakankshaagr2018@gmail.com>
2024-03-26 01:57:39 +0530

    renamed views to public

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ const app = express();
 
 // Set up view engine and parsers
 app.set('view engine', 'pug');
-app.set('views', path.join(__dirname, '..', 'views'));
+app.set('views', path.join(__dirname, '..', 'public'));
 
 // For parsing application/json
 app.use(bodyParser.json());
