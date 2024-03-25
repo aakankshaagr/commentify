64c776e999bf56d0ea7ca2e61722d20cd429a798
Aakanksha Agrawal <aakankshaagr2018@gmail.com>
2024-03-26 01:27:44 +0530

    updated packages and index with cors and express

--- index.js
+++ index.js
@@ -2,6 +2,13 @@ require("dotenv").config();
 const app = require('./src/app');
 const port = process.env.PORT || 3000;
 
+const express = require("express");
+
+const app = express();
+const cors = require("cors");
+
+app.use(cors());
+app.use(express.json());
 app.listen(port, () => {
   console.log(`Server listening at http://localhost:${port}`);
 });
